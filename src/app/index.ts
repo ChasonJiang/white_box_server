b0cb537d3d4da03aec9be4d0b3204ae987871aef
ChasonJiang <1440499136@qq.com>
2021-12-17 12:49:27 +0800

    modified:   src/app/index.ts 	modified:   src/app/interface (new commits)

--- src/app/index.ts
+++ src/app/index.ts
@@ -13,18 +13,10 @@ const db_pool=mysql.createPool({
     database : 'white_box',
     connectionLimit : 100,
 });
-// const db = mysql.createConnection({
-//     host     : '106.52.148.60',
-//     user     : 'white_box',
-//     password : 'Project@white_box_410',
-//     database : 'white_box',
 
-//   });
-  
 app.use(express.json({'limit':'100mb'}));
 app.use(express.urlencoded({extended:false}));
 app.use(cors()); 
-
 app.post('/api',(req:Request, res:Response,next:NextFunction) =>main(req,res,next));
 
 function main(req:Request, res:Response, next:NextFunction){
@@ -32,18 +24,20 @@ function main(req:Request, res:Response, next:NextFunction){
     let body=req.body;
     try{
         TYPE[body.head.type](db_pool,req,res);
-
     }catch(err){
-        res.send("未知类型！");
+        console.log(err);
     }finally{
-        // next();
-    }
 
-    // return;
+    }
 }
 
+process.on('uncaughtException',(err)=>{
+    console.log(err);
+});
 
-
+process.on('unhandkedRejection',(err)=>{
+    console.log(err);
+});
 
 
 app.listen(port,()=>{
